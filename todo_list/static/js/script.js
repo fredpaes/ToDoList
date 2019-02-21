723de7ec9538d98e2a9a033cf91e08341aa8d904
Fred <fred.paucarespinoza@gmail.com>
2019-02-21 13:58:12 -0500

    first commit

---
+++ todo_list/static/js/script.js
@@ -0,0 +1,7 @@
+M.AutoInit();
+
+// NAVBAR mobile
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
