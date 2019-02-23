319263c2cb0e02ce429f057428f7c874a6547a6b
Fred <fred.paucarespinoza@gmail.com>
2019-02-22 21:35:48 -0500

    view add

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class List(models.Model):
+    LOCATION = (
+        ('out', 'exterior'),
+        ('in', 'interior'),
+    )
+    item = models.CharField(max_length=100)
+    location = models.CharField(max_length=3, choices=LOCATION)
+    cant = models.FloatField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.item + ' | ' + str(self.completed)
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from .models import *
+from .forms import ListForm
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    all_items = List.objects.all
+    return render(request, 'home.html', {'todos': all_items})
 
 def add(request):
-    return render(request, 'add.html', {})
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            messages.success(request, ('Item has been added to list!'))
+            return render(request, 'add.html', {'all_items': all_items})
+    else:
+        all_items = List.objects.all
+        return render(request, 'add.html', {'all_items': all_items})
