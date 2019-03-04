a53316af27abcc460b9ad948c54ee2d26e1f3f5e
Fred <fred.paucarespinoza@gmail.com>
2019-03-04 16:16:32 -0500

    log in added

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
+# from django.http import HttpResponseRedirect
 from .models import List
 from .forms import ListForm
 from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    all_items = List.objects.all
-    return render(request, 'home.html', {'all_items': all_items})
+    if request.user.is_authenticated:
+        all_items = List.objects.all
+        return render(request, 'home.html', {'all_items': all_items})
+    else:
+        return redirect('login')
 
 def add(request):
-    if request.method == 'POST':
-        form = ListForm(request.POST)
-        
-        if form.is_valid():
-            form.save()
-            # all_items = List.objects.all
-            # messages.success(request, ('success'))
-            # return render(request, 'home.html', {'all_items': all_items})
-            return HttpResponseRedirect('home')
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = ListForm(request.POST)
+            
+            if form.is_valid():
+                form.save()
+                # all_items = List.objects.all
+                # messages.success(request, ('success'))
+                # return render(request, 'home.html', {'all_items': all_items})
+                return redirect('home')
+        else:
+            return render(request, 'add.html')
     else:
-        return render(request, 'add.html')
+        return redirect('login')
---
+++ users/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate, logout
+
+# Create your views here.
+def log_in(request):
+    error_messages = []
+    if request.user.is_anonymous:
+        if request.method == 'POST':
+            usuario = request.POST.get('user_name', '')
+            passwrd = request.POST.get('user_pwd')
+
+            user = authenticate(username=usuario, password=passwrd)
+            if user is None:
+                error_messages.append('No hay usuario.')
+            elif user.is_active:
+                login(request, user)
+                return redirect('home')
+            else:
+                error_messages.append('No está activo')
+
+        context = {'msgs': error_messages}
+        return render(request, 'login.html', context)
+    else:
+        # redirect a la vista lista
+        return redirect('home')
+
+def log_out(request):
+    if request.user.is_authenticated:
+        logout(request)
+    
+    return redirect('login')
