88feb9583c575e1621fb25ba596a08b382f9caf6
Fred <fred.paucarespinoza@gmail.com>
2019-03-09 21:37:11 -0500

    unifying staticfiles

--- todo_list/static/js/script.js
+++ todo_list/static/js/script.js
@@ -4,4 +4,8 @@ M.AutoInit();
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
-  });
+});
+
+// add.html
+const cnt = document.getElementById('toDoCnt')
+cnt.value = 0
